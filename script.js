9e00a62d5b091ee8a0dd121d7d922653f713ba39
Abdubalievazamat <azikds2008@gmail.com>
2023-01-24 19:00:18 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const arr = [ "1", "43", "23", "54", "2", "3", "89"]
+// const str = arr.join(' ')
+// console.log(str)
+
+//
+// let max = arr[0]
+// let min = arr[0]
+// for (let i = 0; i<arr.length; i++) {
+//     if(max<arr[i]){
+//         max=arr[i]
+//     }
+//     if(min>arr[i]){
+//         min=arr[i]
+//     }
+// }
+// console.log(max)
+// console.log(min)
+
+
+// let even = 0
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         even += arr[i]
+//     }
+// }
+// console.log(even)
+
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     console.log(arr[i])
+// }
+
+
+// const arr2 = arr.join(",")
+// console.log(arr2)
+
+//
+// const str = arr.join(" ")
+// for (let i= 0; i<1; i++) {
+//     for (let j= 0; i<1; j++) {
+//
+//     }
+// }
+
+
